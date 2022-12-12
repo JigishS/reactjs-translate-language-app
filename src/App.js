3937b88b553af93a806a717476fa4fa57f19ff72
Jigish Solanki <jigishsolanki111@gmail.com>
2022-12-12 20:28:28 +0530

    Translate entred text

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { useState } from 'react';
 import './App.css';
+import Field from './components/Field';
+import Languages from './components/Languages';
+import Translate from './components/Translate';
 
 function App() {
+  const [language, setLanguage] = useState('es');
+  const [text, setText] = useState('');
+
   return (
-    <div className="App">
-      <p>
-        Hello World
-      </p>
+    <div>
+      <Field onChange={setText}/>
+      <Languages language={language} onLanguageChange={setLanguage} />
+      <hr />
+      <Translate text={text} language={language} />
     </div>
   );
 }
